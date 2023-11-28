de36e4fa2eefe300aca118529c0f2d6ce53fdbf1
Krithik Singh <krithiksingh@gmail.com>
2023-11-28 10:08:41 -0500

    static methods lab completed

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 class Formatter {
-  //add static methods here
+    //add static methods here
+    static capitalize(string) {
+        const upperCasedString = string[0].toUpperCase() + string.slice(1);
+        return upperCasedString;
+    }
+    static sanitize(string) {
+        const sanitizedString = string.replace(/[^\w\s'-]/g, '');
+        return sanitizedString;
+    }
+    static titleize(string) {
+        const notCapital = [
+            'the',
+            'a',
+            'an',
+            'but',
+            'of',
+            'and',
+            'for',
+            'at',
+            'by',
+            'from',
+        ];
+
+        const words = string.split(' ');
+
+        const titleizedWords = words.map((word, index) => {
+            if (index === 0 || !notCapital.includes(word.toLowerCase())) {
+                return word.charAt(0).toUpperCase() + word.slice(1);
+            } else {
+                return word;
+            }
+        });
+        const titleizedString = titleizedWords.join(' ');
+
+        return titleizedString;
+    }
 }
